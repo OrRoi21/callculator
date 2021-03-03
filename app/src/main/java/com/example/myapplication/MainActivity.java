17b12891b3c0312bae721f9715bdbfb493924e79
orIsc <orjslash27@gmail.com>
2021-03-03 18:06:28 +0200

    build calculatore view and add numbers functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setText("");
     }
 
+    public void writeNumber(View view) {
+        Button b = (Button) view;
+        textView.append(b.getText());
+
+    }
 }
