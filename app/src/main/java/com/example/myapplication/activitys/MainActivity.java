ca986938ff1afc5ae6e5857609a201597e992590
orIsc <orjslash27@gmail.com>
2021-05-01 22:51:53 +0300

    fix server functionality

--- app/src/main/java/com/example/myapplication/activitys/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activitys/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.myapplication.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.media.VolumeShaper;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,19 +12,20 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.Math;
+import java.io.ObjectOutputStream;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import com.example.myapplication.dm.Request;
 import com.example.myapplication.R;
-import com.example.myapplication.dm.Request;
 import com.example.myapplication.fragments.ScienceFragment;
 import com.example.myapplication.fragments.SimpleFragment;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
     private static Button b;
     private static int result = 0;
     private boolean isOn = false;
-    private Request request;
+    private static String reqStr = null;
+    private static String resultStr;
+    private static Request request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,25 +70,43 @@ public class MainActivity extends AppCompatActivity {
             frameLayout.setVisibility(View.VISIBLE);
             isOn = !isOn;
         }
-        request = new Request(num1, num2, op);
-        new DoingBackground().execute(request);
+        new DoingBackground();
         fragmentTransaction.commit();
 
     }
 
-    private class DoingBackground extends AsyncTask<Request, Void, String> {
+    private static class DoingBackground extends AsyncTask<Request, Void, String> {
 
         @Override
         protected String doInBackground(Request... requests) {
             try {
                 Socket socket = new Socket("10.100.102.7", 12345);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ObjectOutputStream writer = new ObjectOutputStream(socket.getOutputStream());
+                reqStr = writeRequest(requests[0]);
+                Log.d("to server", reqStr);
+                writer.writeObject(reqStr);
+                resultStr = reader.readLine();
+
             }catch(ConnectException e) {
                 System.out.println(e.getMessage());
             } catch(IOException e) {
                 System.out.println(e.getMessage());
             }
 
-            return null;
+            return resultStr;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(resultStr);
+            if(!textView.getText().toString().equals("Error")) {
+                textView.setText(resultStr);
+                num1 = Integer.parseInt(resultStr);
+                op = ' ';
+            }
         }
     }
 
@@ -125,7 +143,12 @@ public class MainActivity extends AppCompatActivity {
     public static void getOperator(View view) {
         b = (Button) view;
         op = b.getText().charAt(0);
-        textView.append(b.getText());
+        if(op == '^' || op == 'r') {
+            request = new Request(num1, num2, op);
+            new DoingBackground().execute(request);
+        }else {
+            textView.append(b.getText());
+        }
     }
 
     public static void clearTextfield(View view) {
@@ -141,39 +164,9 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Num" , temp2);
         num2 = Integer.parseInt(temp2);
 
+        request = new Request(num1, num2, op);
+        new DoingBackground().execute(request);
 
-        switch (op) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if(num2 == 0){
-                    textView.setText((String)"Error");
-                    num1 = 0;
-                }else
-                    result = num1 / num2;
-                break;
-            case '^':
-                result = num1 * num1;
-                break;
-            case 'r':
-                result = (int) Math.sqrt(num1);
-                break;
-            default:
-                break;
-
-        }
-        if(!textView.getText().toString().equals("Error")) {
-            textView.setText(Integer.toString(result));
-            num1 = result;
-            op = ' ';
-        }
     }
 
     public void loadSetFragment() {
@@ -190,6 +183,13 @@ public class MainActivity extends AppCompatActivity {
             frameLayout.setVisibility(View.GONE);
         }
         fragmentTransaction.addToBackStack(null).commit();
+    }
+
+    public static String writeRequest(Request request) {
+        String reqStr = null;
+        Gson gson = new Gson();
+        reqStr = gson.toJson(request);
+        return reqStr;
     }
 
 
